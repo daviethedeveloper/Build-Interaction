31a1e99563bf2caeedbfcaa61f9ce52607f69040
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2023-01-25 18:53:26 -0700

    First Commit
    - Brought in Boiler Plate
    - Imported Solana Library
    
    ToDo
    - Write on index.ts file interaction

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// We're adding these
+import * as Web3 from '@solana/web3.js';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function main() {
+}
+
+main()
+  .then(() => {
+    console.log('Finished successfully');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
